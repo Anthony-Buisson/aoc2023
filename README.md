30a79b193868fb218e7572c88a634b42b999d596
Anthony Buisson <anthony.buisson@gokepler.fr>
2023-12-06 10:05:28 +0100

    jour 6 🤡

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
-![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
@@ -97,16 +97,16 @@ Both parts: 4888.627ms
 
 ```
 Day 05
-Time part 1: 0.481ms
-Time part 2: 664420.724ms
-Both parts: 664421.205ms
+Time part 1: 0.867ms
+Time part 2: 653435.681ms
+Both parts: 653436.548ms
 ```
 
 ```
 Day 06
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.141ms
+Time part 2: 62.32ms
+Both parts: 62.461ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 10/50
-Total time: 669317.976ms
+Total stars: 12/50
+Total time: 658395.779ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day06/index.ts
@@ -0,0 +1,57 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => {
+    const timeAndDistance = rawInput.match(/(\d+)/g).map(n => Number(n)); // [ ...times, ...distances ]
+    return [...timeAndDistance]
+};
+
+const part1 = (rawInput: string) => {
+    const input = parseInput(rawInput);
+    const nbRaces = input.length / 2
+    const numberOfWaysToWin = Array(nbRaces).fill(0);
+    for (let i = 0; i < nbRaces; i++) {
+        for (let j = 1; j < input[i]; j++) {
+            if (j * (input[i] - j) > input[i + nbRaces]) {
+                numberOfWaysToWin[i] += 1
+            }
+        }
+    }
+
+    return numberOfWaysToWin.reduce((acc, val) => acc * val, 1);
+};
+
+const part2 = (rawInput: string) => {
+    const input = parseInput(rawInput);
+    let numberOfWaysToWin = 0;
+    const time = Number(input.slice(0, input.length / 2).reduce((acc, val) => acc + String(val), ''))
+    const distance = Number(input.slice(input.length / 2).reduce((acc, val) => acc + String(val), ''))
+    for (let i = 0; i < time; i++) {
+        if (i * (time - i) > distance) {
+            numberOfWaysToWin += 1
+        }
+    }
+    return numberOfWaysToWin;
+};
+
+run({
+    part1: {
+        tests: [
+            // {
+            //   input: ``,
+            //   expected: "",
+            // },
+        ],
+        solution: part1,
+    },
+    part2: {
+        tests: [
+            // {
+            //   input: ``,
+            //   expected: "",
+            // },
+        ],
+        solution: part2,
+    },
+    trimTestInputs: true,
+    onlyTests: false,
+});
